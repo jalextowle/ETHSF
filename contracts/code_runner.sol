6dcf06656df039d4e651042eaadbeb513641d082
James Towle <jalextowle@berkeley.edu>
2018-10-06 15:27:55 -0700

    Wrote out the jump table for the run function. This will be refactored when the correct jump table is known

--- contracts/code_runner.sol
+++ contracts/code_runner.sol
@@ -80,7 +80,83 @@ contract Runner {
         mstore(stack_ptr, addmod(mload(add(stack_ptr, 0x40)), mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
         stack_ptr := add(stack_ptr, 0x20)
         jump(LOOP)
-
+      $MULMOD:
+        stack_ptr := safe_sub(stack_ptr, 0x60, stack_start)
+        mstore(stack_ptr, mulmod(mload(add(stack_ptr, 0x40)), mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
+      $EXP:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, exp(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
+      $SIGNEXTEND:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, signextend(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
+      $LT:
+      $GT:
+      $SLT:
+      $SGT:
+      $EQ:
+      $ISZERO:
+      $AND:
+      $OR:
+      $XOR:
+      $NOT:
+      $BYTE:
+      $SHA3:
+      $ADDRESS:
+      $BALANCE:
+      $ORIGIN:
+      $CALLER:
+      $CALLVALUE:
+      $CALLDATALOAD:
+      $CALLDATASIZE:
+      $CALLDATACOPY:
+      $CODESIZE:
+      $CODECOPY:
+      $GASPRICE:
+      $EXTCODESIZE:
+      $EXTCODECOPY:
+      $RETURNDATASIZE:
+      $RETURNDATACOPY:
+      $BLOCKHASH:
+      $COINBASE:
+      $TIMESTAMP:
+      $NUMBER:
+      $DIFFICULTY:
+      $GASLIMIT:
+      $POP:
+      $MLOAD:
+      $MSTORE:
+      $MSTORE8:
+      $SLOAD:
+      $SSTORE:
+      $JUMP:
+      $JUMPI:
+      $PC:
+      $MSIZE:
+      $GAS:
+      $JUMPDEST:
+      $PUSH:
+      $DUP:
+      $SWAP:
+      $LOG0:
+      $LOG1:
+      $LOG2:
+      $LOG3:
+      $LOG4:
+      $CREATE:
+      $CALL:
+      $CALLCODE:
+      $RETURN:
+      $DELEGATECALL:
+      $STATICCALL:
+      $REVERT:
+      $INVALID:
+      $SELFDESTRUCT:
 
       function safe_sub(_ptr, _dif, _ref) -> ptr {
         ptr := sub(_ptr, _dif)
@@ -97,15 +173,92 @@ contract Runner {
       }
 
       function place_table() {
-        mstore(0x80, $STOP)
-        mstore(0x82, $ADD)
-        mstore(0x84, $MUL)
-        mstore(0x86, $SUB)
-        mstore(0x88, $DIV)
-        mstore(0x90, $SDIV)
-        mstore(0x92, $MOD)
+        mstore(0x62, $STOP)
+        mstore(0x64, $ADD)
+        mstore(0x66, $MUL)
+        mstore(0x68, $SUB)
+        mstore(0x6a, $DIV)
+        mstore(0x6c, $SDIV)
+        mstore(0x6e, $MOD)
+        mstore(0x70, $SMOD)
+        mstore(0x72, $ADDMOD)
+        mstore(0x74, $MULMOD)
+        mstore(0x76, $EXP)
+        mstore(0x78, $SIGNEXTEND)
+        mstore(0x82, $LT)
+        mstore(0x84, $GT)
+        mstore(0x86, $SLT)
+        mstore(0x88, $SGT)
+        mstore(0x8a, $EQ)
+        mstore(0x8c, $ISZERO)
+        mstore(0x8e, $AND)
+        mstore(0x90, $OR)
+        mstore(0x92, $XOR)
+        mstore(0x94, $NOT)
+        mstore(0x96, $BYTE)
+        mstore(0xa2, $SHA3)
+        mstore(0xc2, $ADDRESS)
+        mstore(0xc4, $BALANCE)
+        mstore(0xc6, $ORIGIN)
+        mstore(0xc8, $CALLER)
+        mstore(0xca, $CALLVALUE)
+        mstore(0xcc, $CALLDATALOAD)
+        mstore(0xce, $CALLDATASIZE)
+        mstore(0xd0, $CALLDATACOPY)
+        mstore(0xd2, $CODESIZE)
+        mstore(0xd4, $CODECOPY)
+        mstore(0xd6, $GASPRICE)
+        mstore(0xd8, $EXTCODESIZE)
+        mstore(0xda, $EXTCODECOPY)
+        mstore(0xdc, $RETURNDATASIZE)
+        mstore(0xde, $RETURNDATACOPY)
+        mstore(0xe2, $BLOCKHASH)
+        mstore(0xe4, $COINBASE)
+        mstore(0xe6, $TIMESTAMP)
+        mstore(0xe8, $NUMBER)
+        mstore(0xea, $DIFFICULTY)
+        mstore(0xec, $GASLIMIT)
+        mstore(0x102, $POP)
+        mstore(0x104, $MLOAD)
+        mstore(0x106, $MSTORE)
+        mstore(0x108, $MSTORE8)
+        mstore(0x10a, $SLOAD)
+        mstore(0x10c, $SSTORE)
+        mstore(0x10e, $JUMP)
+        mstore(0x110, $JUMPI)
+        mstore(0x112, $PC)
+        mstore(0x114, $MSIZE)
+        mstore(0x116, $GAS)
+        mstore(0x118, $JUMPDEST)
+        for { let i := 0x122 } lt(i, 0x15c) { i := add(0x2) }
+        {
+          mstore(i, $PUSH)
+        }
+        for { let i := 0x162 } lt(i, 0x17c) { i := add(0x2) } 
+        {
+          mstore(i, $DUP)
+        }
+        for { let i := 0x182 } lt(i, 0x19c) { i := add(0x2) } 
+        {
+          mstore(i, $SWAP)
+        }
+        mstore(0x1a2, $LOG0)
+        mstore(0x1a4, $LOG1)
+        mstore(0x1a6, $LOG2)
+        mstore(0x1a8, $LOG3)
+        mstore(0x1aa, $LOG4)
+
+        mstore(0x242, $CREATE)
+        mstore(0x244, $CALL)
+        mstore(0x246, $CALLCODE)
+        mstore(0x248, $RETURN)
+        mstore(0x24a, $DELEGATECALL)
+        mstore(0x256, $STATICCALL)
+        mstore(0x25c, $REVERT)
+        mstore(0x25e, $INVALID)
+        mstore(0x260, $SELFDESTRUCT)
+
       }
     }
   }
-
 }
