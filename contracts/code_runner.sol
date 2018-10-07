80729d3394ee2987c8cca835ac9ed0a15feb54c9
James Towle <jalextowle@berkeley.edu>
2018-10-06 19:29:24 -0700

    Implemented all of the EVM opcodes with the exception of PUSH, DUP, and SWAP

--- contracts/code_runner.sol
+++ contracts/code_runner.sol
@@ -8,26 +8,25 @@ contract Runner {
 
   function run(bytes memory _code, bytes memory _data) public payable {
     assembly {
-      // FIXME - Place jump table in memory
       place_table()
-
       // Instantiate the opcode and label variables
       let opcode
       let label
       // Set the code_ptr to point after _code's length slot
       let code_ptr := add(_code, 0x1f) 
+      let code_size := mload(_code)
       // Set the code_end value to point after the bytecode
-      let code_end := add(code_ptr, mload(_code)) 
+      let code_end := add(code_ptr, add(code_size, 0x1)) 
       // Set the start of the stack to the current free memory pointer
       let stack_start := mload(0x40)
       // Set the stack pointer to the start of the stack
       let stack_ptr := stack_start 
       // Create space in memory for the stack
       let mem_ptr := add(stack_ptr, mul(1024, 0x20))
+      let cd_ptr := add(calldataload(0x24), 0x20)
       // Set up the pseudo free memory pointer
       mstore(add(mem_ptr, 0x40), 0x80)
-
-      // Main Loop
+      // Main LOOP
       LOOP:
         code_ptr := add(code_ptr, 0x1) 
         if gt(code_ptr, code_end) {
@@ -36,8 +35,11 @@ contract Runner {
         opcode := byte(mload(code_ptr), 0x0) 
         // Load the jumpdest for the given opcode from the jump table
         label := div(mload(add(mul(opcode, 0x2), 0x80)), exp(2, 240))
+        // If the opcode's label equals zero, revert because the opcode is not in the jump table
+        if eq(label, 0x0) {
+          revert(0x0, 0x0)
+        }
         jump(label) 
-
       $STOP: 
         stop()
       $ADD:
@@ -96,67 +98,369 @@ contract Runner {
         stack_ptr := add(stack_ptr, 0x20)
         jump(LOOP)
       $LT:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, lt(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $GT:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, gt(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $SLT:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, slt(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $SGT:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, sgt(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $EQ:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, eq(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $ISZERO:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        mstore(stack_ptr, iszero(mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $AND:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, and(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $OR:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, or(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $XOR:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, xor(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $NOT:
+        stack_ptr := safe(stack_ptr, 0x20, stack_start)
+        mstore(stack_ptr, not(mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $BYTE:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, byte(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $SHA3:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(stack_ptr, keccak256(mload(add(stack_ptr, 0x20)), mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $ADDRESS:
+        mstore(stack_ptr, address)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $BALANCE:
+        mstore(stack_ptr, balance)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $ORIGIN:
+        mstore(stack_ptr, origin)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $CALLER:
+        mstore(stack_ptr, origin)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $CALLVALUE:
+        mstore(stack_ptr, origin)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $CALLDATALOAD:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        mstore(
+          stack_ptr, 
+          calldataload(
+            add(mload(stack_ptr), cd_ptr)
+          )
+        )
+        jump(LOOP)
       $CALLDATASIZE:
+        mstore(stack_ptr, origin)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $CALLDATACOPY:
+        stack_ptr := safe_sub(stack_ptr, 0x60, stack_start)
+        calldatacopy(
+          add(mload(add(stack_ptr, 0x40)), mem_ptr),
+          add(mload(add(stack_ptr, 0x20)), cd_ptr), 
+          mload(stack_ptr)
+        ) 
+        jump(LOOP)
       $CODESIZE:
+        mstore(stack_ptr, mload(_code))
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $CODECOPY:
+        stack_ptr := safe_sub(stack_ptr, 0x60, stack_start)
+        let code_start := add(mload(add(stack_ptr, 0x20)), 0x64)
+        let size := mload(stack_ptr)
+        if gt(add(code_start, size), code_end) {
+          calldatacopy(
+            add(mload(add(stack_ptr, 0x40)), mem_ptr),
+            code_start,
+            sub(code_end, code_start)
+          )
+          codecopy(
+            add(add(mload(add(stack_ptr, 0x40)), mem_ptr), sub(code_end, code_start)), 
+            codesize, 
+            sub(size, sub(code_end, code_start))
+          )         
+          jump(LOOP) 
+        }
+        calldatacopy(
+          add(mload(add(stack_ptr, 0x40)), mem_ptr),
+          code_start,
+          size
+        )
+        jump(LOOP)
       $GASPRICE:
+        mstore(stack_ptr, gasprice)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $EXTCODESIZE:
+        mstore(stack_ptr, extcodesize(mload(stack_ptr)))
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        jump(LOOP)
       $EXTCODECOPY:
+        stack_ptr := safe_sub(stack_ptr, 0x80, stack_start)
+        extcodecopy( 
+          mload(add(stack_ptr, 0x60)), 
+          add(mload(add(stack_ptr, 0x40), mem_ptr), 
+          mload(add(stack_ptr, 0x20)),
+          mload(stack_ptr)
+        )
+        jump(LOOP)
       $RETURNDATASIZE:
+        mstore(stack_ptr, returndatasize)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $RETURNDATACOPY:
+        stack_ptr := safe_sub(stack_ptr, 0x60, stack_start)
+        returndatacopy(
+          add(mload(add(stack_ptr, 0x40)), mem_ptr), 
+          mload(add(stack_ptr, 0x20)), 
+          mload(stack_ptr)
+        ) 
+        jump(LOOP)
       $BLOCKHASH:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        mstore(stack_ptr, blockhash(mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $COINBASE:
+        mstore(stack_ptr, coinbase)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $TIMESTAMP:
+        mstore(stack_ptr, timestamp)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $NUMBER:
+        mstore(stack_ptr, number)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $DIFFICULTY:
+        mstore(stack_ptr, difficulty)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $GASLIMIT:
+        mstore(stack_ptr, gaslimit)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $POP:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        jump(LOOP)
       $MLOAD:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        mstore(stack_ptr, mload(stack_ptr))
+        jump(LOOP)
       $MSTORE:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore(add(mload(add(stack_ptr, 0x20)), mem_ptr), mload(stack_ptr))
+        jump(LOOP)
       $MSTORE8:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        mstore8(add(mload(add(stack_ptr, 0x20)), mem_ptr), mload(stack_ptr))
+        jump(LOOP)
       $SLOAD:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        mstore(stack_ptr, sload(mload(stack_ptr)))
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $SSTORE:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        sstore(mload(add(stack_ptr, 0x40)), mload(stack_ptr))
+        jump(LOOP)
       $JUMP:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        code_ptr := add(_code, add(0x1f, mload(stack_ptr)))
+        opcode := byte(mload(add(code_ptr, 0x1)), 0x0)
+        if iszero(eq(0x5b, opcode)) {
+          revert(0x0, 0x0)
+        }
+        jump(LOOP)
       $JUMPI:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        if mload(stack_ptr) {
+          code_ptr := add(_code, add(0x1f, mload(add(stack_ptr, 0x20))))
+          opcode := byte(mload(add(code_ptr, 0x1)), 0x0)
+          if iszero(eq(0x5b, opcode)) {
+            revert(0x0, 0x0)
+          }
+        }
+        jump(LOOP)
       $PC:
+        mstore(stack_ptr, pc)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $MSIZE:
+        mstore(stack_ptr, msize)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $GAS:
+        mstore(stack_ptr, gas)
+        stack_ptr := safe_add(stack_ptr, 0x20, mem_ptr)
+        jump(LOOP)
       $JUMPDEST:
+        jump(LOOP)
       $PUSH:
+        jump(LOOP)
       $DUP:
+        jump(LOOP)
       $SWAP:
+        jump(LOOP)
       $LOG0:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        log0(
+           mload(stack_ptr)
+        )
+        jump(LOOP)
       $LOG1:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        log1(
+           mload(add(stack_ptr, 0x20)),
+           mload(stack_ptr)
+        )
+        jump(LOOP)
       $LOG2:
+        stack_ptr := safe_sub(stack_ptr, 0x60, stack_start)
+        log2(
+           mload(add(stack_ptr, 0x40)),
+           mload(add(stack_ptr, 0x20)),
+           mload(stack_ptr)
+        )
+        jump(LOOP)
       $LOG3:
+        stack_ptr := safe_sub(stack_ptr, 0x80, stack_start)
+        log3(
+           mload(add(stack_ptr, 0x60)),
+           mload(add(stack_ptr, 0x40)),
+           mload(add(stack_ptr, 0x20)),
+           mload(stack_ptr)
+        )
+        jump(LOOP)
       $LOG4:
+        stack_ptr := safe_sub(stack_ptr, 0xa0, stack_start)
+        log4(
+           mload(add(stack_ptr, 0x80)),
+           mload(add(stack_ptr, 0x60)),
+           mload(add(stack_ptr, 0x40)),
+           mload(add(stack_ptr, 0x20)),
+           mload(stack_ptr)
+        )
+        jump(LOOP)
       $CREATE:
+        stack_ptr := safe_sub(stack_ptr, 0x60, stack_start)
+        mstore(stack_ptr, 
+               create(
+                 mload(add(stack_ptr, 0x40)),
+                 mload(add(stack_ptr, 0x20)),
+                 mload(stack_ptr)
+               )
+              )
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $CALL:
+        stack_ptr := safe_sub(stack_ptr, 0xe0, stack_start)
+        mstore(stack_ptr, 
+               call(
+                 mload(add(stack_ptr, 0xc0)),
+                 mload(add(stack_ptr, 0xa0)), 
+                 mload(add(stack_ptr, 0x80)),
+                 mload(add(stack_ptr, 0x60)),
+                 mload(add(stack_ptr, 0x40)),
+                 mload(add(stack_ptr, 0x20)),
+                 mload(stack_ptr)
+               )
+              )
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $CALLCODE:
+        stack_ptr := safe_sub(stack_ptr, 0xc0, stack_start)
+        mstore(stack_ptr, 
+               callcode(
+                 mload(add(stack_ptr, 0xa0)), 
+                 mload(add(stack_ptr, 0x80)),
+                 mload(add(stack_ptr, 0x60)),
+                 mload(add(stack_ptr, 0x40)),
+                 mload(add(stack_ptr, 0x20)),
+                 mload(stack_ptr)
+               )
+              )
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $RETURN:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        return(mload(add(stack_ptr, 0x20)), mload(stack_ptr))
       $DELEGATECALL:
+        stack_ptr := safe_sub(stack_ptr, 0xc0, stack_start)
+        mstore(stack_ptr, 
+               delegatecall(
+                 mload(add(stack_ptr, 0xa0)), 
+                 mload(add(stack_ptr, 0x80)),
+                 mload(add(stack_ptr, 0x60)),
+                 mload(add(stack_ptr, 0x40)),
+                 mload(add(stack_ptr, 0x20)),
+                 mload(stack_ptr)
+               )
+              )
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $STATICCALL:
+        stack_ptr := safe_sub(stack_ptr, 0xc0, stack_start)
+        mstore(stack_ptr, 
+               staticcall(
+                 mload(add(stack_ptr, 0xa0)), 
+                 mload(add(stack_ptr, 0x80)),
+                 mload(add(stack_ptr, 0x60)),
+                 mload(add(stack_ptr, 0x40)),
+                 mload(add(stack_ptr, 0x20)),
+                 mload(stack_ptr)
+               )
+              )
+        stack_ptr := add(stack_ptr, 0x20)
+        jump(LOOP)
       $REVERT:
+        stack_ptr := safe_sub(stack_ptr, 0x40, stack_start)
+        revert(add(mload(add(stack_ptr, 0x20)), mem_ptr), mload(stack_ptr))
       $INVALID:
+        invalid
       $SELFDESTRUCT:
+        stack_ptr := safe_sub(stack_ptr, 0x20, stack_start)
+        selfdestruct(mload(stack_ptr))
+        jump(LOOP)
 
       function safe_sub(_ptr, _dif, _ref) -> ptr {
         ptr := sub(_ptr, _dif)
@@ -247,7 +551,6 @@ contract Runner {
         mstore(0x1a6, $LOG2)
         mstore(0x1a8, $LOG3)
         mstore(0x1aa, $LOG4)
-
         mstore(0x242, $CREATE)
         mstore(0x244, $CALL)
         mstore(0x246, $CALLCODE)
@@ -257,7 +560,6 @@ contract Runner {
         mstore(0x25c, $REVERT)
         mstore(0x25e, $INVALID)
         mstore(0x260, $SELFDESTRUCT)
-
       }
     }
   }
